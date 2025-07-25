101c543d555942ccde0bf5f2454b911d18852c84
Erasyl <erasil.bakhytgan@gmail.com>
2025-07-25 16:11:11 +0500

    Compelete migrations

--- AISERVICE/projects/models.py
+++ AISERVICE/projects/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from users.models import AbstractUser
+from users.models import User
 # Create your models here.
 
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    owner = models.ForeignKey(AbstractUser, on_delete=models.CASCADE, related_name='projects')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
